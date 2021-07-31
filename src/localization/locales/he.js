0362d909003edfe2877b4f17d80c8e242278245e
Shahar Eliyahu <65499638+ShaharEli@users.noreply.github.com>
2021-07-31 16:34:15 +0300

    Create he.js

---
+++ src/localization/locales/he.js
@@ -0,0 +1,64 @@
+export default {
+  translation: {
+    routes: {
+      favorites: 'מועדפים',
+      favoritesHeader: 'תחנות מועדפות',
+      stations: 'תחנות',
+      stationsHeader: 'תחנות אוטובוסים',
+      timetable: 'לוח זמנים',
+      lines: 'קווים',
+      linesHeader: 'כיוון קווים',
+      linesMap: 'מתפת קווי אוטבוסים',
+      about: 'אודות',
+      aboutHeader: 'אודות האפליקציה',
+      feedback: 'ביקורת'
+    },
+    timetable: {
+      station: 'מזהה תחנה:',
+      bus: 'אוטובוס',
+      direction: 'קיוון',
+      time: 'זמן',
+      nowText: 'כרגע',
+      minText: 'דקה',
+      favorite: 'קיים כבר ברשימת המועפים',
+      error: 'תקלה קרתה',
+      server_err: 'בעיית שרת זמנית אנא נסה שוב מאוחר יותר',
+      cancel: 'בטל',
+      localTime: 'זמן גאורגיה',
+      localTimeDelay: 'זמן איחור משוער הוא 30+ דק',
+      localTimeNight: "האוטובוס לא פעיל כרגע עקב שעות לילה!"
+    },
+    favorites: {
+      modal: 'האם למחוק תחנה זו?',
+      modalButtonYes: 'יש!',
+      modalButtonNo: 'בטל',
+      empty: 'הוסף כוכב לתחנה על מנת לראות אותה פה'
+    },
+    lines: {
+      bus: 'אוטובוס',
+      direction: 'כיוון הקו',
+      from: 'מתחיל מ:',
+      change: 'שנה כיוון'
+    },
+    about: {
+      info: "ראה זמני הגעות מעודכנים בהתבססות על מיקום המכשיר",
+      changeLanguage: 'שנה שפה',
+      madeBy: 'Made by Davituri. ©',
+      feedbackButton: 'ביקורת'
+    },
+    feedback: {
+      info: "דיווח על באג, הצטרף על מנת לתרום",
+      namePlaceholder: 'הקלד את שמך',
+      emailPlaceholder: 'אימייל',
+      messagePlaceholder: 'הודעה',
+      submitTitle: 'אשר',
+      schemaRequiredName: 'חובה להקליד שם',
+      schemaRequiredEmail: 'חובה להקליד אימייל',
+      schemaRequiredMessage: 'חובה להקליד הודעה',
+      onSuccessfulSubmit: 'תודה על הביקורת!',
+      onSubmitError: 'לא התאפשר לשלוח הודעה',
+      error: 'בעיה קרתה',
+      cancel: 'בטל'
+    }
+  }
+}
